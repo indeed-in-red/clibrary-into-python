07fba6b4038ad0f32322edce53a2a898659c44b1
Dihellgo <92475915+Dihellgo@users.noreply.github.com>
2022-06-06 19:17:34 +0200

    update

--- compilec.py
+++ compilec.py
@@ -1,8 +1,11 @@
+from os import path
 from sys import argv
 from typing import List
 
 def main(args:List[str]) -> None:
-    print(args)
+    directory = path.dirname(path.abspath(__file__))
+    print(directory)
+    #file = getFileName(args)
 
 if __name__ == '__main__':
     main(argv)
---
+++ compilec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 compilec.py $@
+
+exit 0
