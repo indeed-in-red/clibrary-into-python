ff5331a2ab97e2dc9523cfead8017d12ac8bfc04
Dihellgo <92475915+Dihellgo@users.noreply.github.com>
2022-06-11 20:40:48 +0200

    Update

--- compilec.py
+++ compilec.py
@@ -2,10 +2,22 @@ from os import path
 from sys import argv
 from typing import List
 
+def getAllFiles(directory:str) -> List[str]:
+    
+    return files
+
+def getFileName(args:List[str]) -> List[str]:
+    files = []
+    if '-all' in args:
+        files = getAllFiles(directory)
+    for element in args:
+        if not element.startswith('-'):
+            files.append(element)
+    return files
+
 def main(args:List[str]) -> None:
     directory = path.dirname(path.abspath(__file__))
-    print(directory)
-    #file = getFileName(args)
+    files = getFileName(args, directory)
 
 if __name__ == '__main__':
-    main(argv)
+    print(getAllFiles('/home/dihellgo'))
